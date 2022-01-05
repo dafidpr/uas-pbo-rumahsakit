38988145fd85dcb3c2628d7ae0b288c0fa4ba445
wahyuwildana <mohaammaddyvaniww@gmail.com>
2022-01-05 17:35:48 +0700

    fixs

--- bin/rumahsakit.dart
+++ bin/rumahsakit.dart
@@ -1,4 +1,4 @@
-import 'package:rumahsakit/rumahsakit.dart' as rumahsakit;
+import "package:rumahsakit/rumahsakit.dart" as rumahsakit;
 
 void main(List<String> arguments) {
   print('Hello world: ${rumahsakit.calculate()}!');

fdbaa791762dc48d2744a5b48f0e044304230e9d
Putri Novitasari <putrinovitasr14@gmail.com>
2022-01-05 17:01:55 +0700

    menambahkan fungsi testing

--- bin/rumahsakit.dart
+++ bin/rumahsakit.dart
@@ -3,3 +3,7 @@ import 'package:rumahsakit/rumahsakit.dart' as rumahsakit;
 void main(List<String> arguments) {
   print('Hello world: ${rumahsakit.calculate()}!');
 }
+
+void testing() {
+  print('text');
+}

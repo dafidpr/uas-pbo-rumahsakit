d65a33863ed97a900078447313c8ae426749ecf6
ardianfirdaus <ardianfirdaus50434@gmail.com>
2022-01-13 23:08:39 +0700

    view

--- bin/Pembayaran/main.dart
+++ bin/Pembayaran/main.dart
@@ -1,70 +1,68 @@
 import 'dart:io';
-import 'dart:convert';
 import 'dart:core';
 
-// class myClass {
-//   final String nama ;
-//   static String nama_obat = 0;
+class myClass {
 //   static int umur = 0;
 //   static int no_telp = 0;
-//   static int harga = 0;
-//   static int totalbayar =0; 
-//   static int totalharga = 0;
 //   static int rawatinap = 0;
 //   static int rawatjalan = 0;
-// }
+}
 
 void  main(List<String> args) {
+  const registrationdate = "Senin 12 Desember 2021";
+  final date = new DateTime.now();
 
   List<List> listPenyakit = [
-    ['1', 'Anemia','Antibiotik dan Immunosupresan', '2000000'],
-    ['2', 'Demam','Paracetamol', '100000'],
+    ['1','Anemia','Antibiotik','2000000'],
+    ['2',' Demam','Paracetamol','100000'],
   ];
 
-  print("-----------------------------------------------");
-  print("-            Hospital Cashier Payment         -");
-  print("-               Disease price data            -");
-  print("-----------------------------------------------");
-  print("-----------------------------------------------");
-  print("- No. Name Disease  Medicine    Harga     -");
-  print("-----------------------------------------------");
+  print("--------------------------------------------------------------------------");
+  print("-                           Hospital Cashier Payment                     -");
+  print("-                              Disease price data                        -");
+  print("--------------------------------------------------------------------------");
+  print("--------------------------------------------------------------------------");
+  print(" No. | Name Disease  | Medicine                    | Harga              ");
+  print("--------------------------------------------------------------------------");
   for(var i = 0; i < listPenyakit.length; i++) {
-    print("${i+1}. ${listPenyakit[i][1]} ${listPenyakit[i][2]} Rp ${listPenyakit[i][3]}");
+    print(" ${i+1}.  | ${listPenyakit[i][1]}       | ${listPenyakit[i][2]}                  | Rp ${listPenyakit[i][3]}");
   }
-  print("-----------------------------------------------");
+  print("--------------------------------------------------------------------------");
   print("Enter the following data :");
   String? nomorPenyakit = stdin.readLineSync();
+  String nmobat ='';
+  for(var i = 0; i < listPenyakit.length; i++) {
+    if(listPenyakit[i][0] == nomorPenyakit) {
+      nmobat = listPenyakit[i][2];
+    }
+  }
   String harga = '';
   for(var i = 0; i < listPenyakit.length; i++) {
     if(listPenyakit[i][0] == nomorPenyakit) {
-      print('Total harganya adalah : ${listPenyakit[i][3]}');
+      print('The total price is :Rp. ${listPenyakit[i][3]}\n');
       harga = listPenyakit[i][3];
     }
   }
 
   if(nomorPenyakit != null){
 
-  print("Masukan jumlah bayar : ");
+  print("Enter the amount paid : Rp.");
   String? jumlahBayar = stdin.readLineSync();
   var kembalian = int.parse(jumlahBayar.toString()) - int.parse(harga);
-  print("Kembalian : Rp ${kembalian}");
+  print("Return : Rp ${kembalian}\n");
   }
 
-  // print("Enter Name : ");
-
-  // print("Enter Length of Stay : ");
-
-  print("Total Pay = Rp ");
-  print("_____________________________");
+  //input data
+  print("Enter Name : ");
+  String? nama = stdin.readLineSync();
   
+  print("\n----------------------------------------");
+  print("-   Payment Note for taking medicine   -");
   print("----------------------------------------");
-  print("- Payment Note for taking medicine -");
-  print("----------------------------------------");
-  print("Medicine name : "); 
-  print("Harga Obat : Rp" );
-  print("Lama Menginap : ");
-  print("Total Harga : Rp");
-  print("Total Bayar = Rp");
-  print("_____________________________");
+  print("Nama : ${nama}");
+  print("Tanggal Sekarang : $date");
+  print("Medicine name :  $nmobat"); 
+  print("Total Pay : Rp. $harga ");
+  print("----------------------------------------\n");
 }
 

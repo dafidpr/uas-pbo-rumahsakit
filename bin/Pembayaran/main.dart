a5676880f4971423dbcbfd37dd001b716511a5c2
ardianfirdaus <ardianfirdaus50434@gmail.com>
2022-01-13 15:11:29 +0700

    fix main

---
+++ bin/Pembayaran/main.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+class myClass {
+  String? nama, umur, no_telp, nama_obat;
+  int? harga, totalbayar, totalharga;
+}
+
+void  main(List<String> args) {
+  print("_____________________________");
+  print("Program Pembayaran Kasir");
+  print("1. Asma dengan harga Rp 200.000,-");
+  print("2. Demam dengan harga Rp 200.000,-");
+  print("_____________________________");
+  print("Masukan Data berikut :");
+  print("Masukkan Nama : ");
+  //stdout.write("Masukkan Total Pembayaran :");
+  print("Masukkan Total Pembayaran :");
+  
+  print("Total harga = Rp " );
+  print("Total Bayar = Rp"  );
+  print("_____________________________");
+  print("Nota Pembayaran ambil obat");
+  print("Nama Obat =");  
+  print("Harga Obat = Rp" );
+  print("Total Harga = Rp");
+  print("Total Bayar =Rp");
+  print("_____________________________");
+}
+
